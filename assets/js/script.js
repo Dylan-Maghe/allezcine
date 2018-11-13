84d2257d3469e6a92a51265a9a0ee1ec2a95468d
Dylan Maghe <maghe.dylan@gmail.com>
2018-11-13 10:41:16 +0100

    ...

--- assets/js/script.js
+++ assets/js/script.js
@@ -69,17 +69,71 @@ let closeUserContact = () => { // fonction ferme le pop up contact us
 
 let contactFormulary = () => { // fonction qui affiche et masque le formulaire de contact à remplir
     if (document.querySelector(".send-message").disabled == true) {
-    document.querySelector(".send-message").disabled = false;
-    document.querySelector("#contact-button").innerText = "CLOSE";
+        document.querySelector(".send-message").disabled = false;
+        document.querySelector("#contact-button").innerText = "CLOSE";
     }
     else {
-    document.querySelector(".send-message").disabled = true;
-    document.querySelector("#contact-button").innerText = "OPEN";  
+        document.querySelector(".send-message").disabled = true;
+        document.querySelector("#contact-button").innerText = "OPEN";
     }
     $(".contact-formulary").toggleClass("contact-formulary-active");
     $(".send-message").toggleClass("send-message-active");
 }
 
+// JEREMY
 
-allowEntry();
+let genderAffichage = (type, categ) => {
+    $(`#${type}-${categ}`).click(function () {
+        $(`.gender-${categ}`).removeClass('active');
+        $(this).addClass('active');
+
+        $(`.featured-${categ}`).hide();
+
+        if (type == 'all') {
+            $(`.featured-${categ}`).show();
+            clic++
+        } else {
+            $(`.${type}-${categ}`).show();
+        }
+    });
+};
+
+// Séries
+genderAffichage('all', 'series');
+genderAffichage('comedies', 'series');
+genderAffichage('police', 'series');
+genderAffichage('autres', 'series');
+// Films
+genderAffichage('all', 'films');
+genderAffichage('action', 'films');
+genderAffichage('comedie', 'films');
+genderAffichage('autres', 'films');
+
+
+let moreAffichage = (classe) => {
+    let tableau = $(`.${classe}`).length;
+
+    for (let i = 12; i < tableau; i++) {
+        $(`.${classe}`).eq(i).hide();
+    }
+
+    $(`#more-${classe}`).click(function () {
+        for (let i = 12; i < tableau; i++) {
+            $(`.${classe}`).eq(i).toggle();
+        }
+
+        if (clic % 2 == 0) {
+            $(this).text('Moins de films');
+        } else {
+            $(this).text('Plus de films');
+        }
+        clic++;
+    })
+}
+
+moreAffichage('featured-films');
+moreAffichage('featured-series');
+
+
+//allowEntry();
 //home/user/Desktop/becode_projects/allezcine/index.html
